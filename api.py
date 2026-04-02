c8271a1b8898811f937163da5353ae14a47d7b5d
Ashish <120723880+Mr-AshishBhandari@users.noreply.github.com>
2026-04-02 18:49:44 +0545

    Add print statement for server port

--- api.py
+++ api.py
@@ -114,4 +114,5 @@ def ask(query,video_id):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 10000))
+    print(f"server running on port {port}.... ")
     uvicorn.run(app, host="0.0.0.0", port=port)
